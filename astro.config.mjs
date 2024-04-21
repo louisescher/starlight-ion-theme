428d4b56bd6358b405b62d3082f9c181ae318feb
Louis Escher <louisescher@proton.me>
2024-04-21 13:36:52 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'Ion Theme',
+    social: {
+      github: 'https://github.com/comet-analytics/ion-theme'
+    },
+    sidebar: [{
+      label: '[home] Home',
+      link: '/'
+    }, {
+      label: '[list] Features',
+      link: '/features/'
+    }, {
+      label: '[box] Guides',
+      autogenerate: {
+        directory: 'guides'
+      }
+    }, {
+      label: '[box] Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }],
+    components: {
+      ThemeProvider: './src/components/ThemeProvider.astro',
+      ThemeSelect: './src/components/ThemeSelect.astro',
+      SiteTitle: './src/components/SiteTitle.astro',
+      Sidebar: './src/components/Sidebar.astro',
+      Pagination: './src/components/Pagination.astro',
+      Hero: './src/components/Hero.astro',
+    },
+    customCss: [
+      '@fontsource-variable/space-grotesk/index.css',
+      '@fontsource/space-mono/400.css',
+      './src/styles/theme.css'
+    ],
+    expressiveCode: {
+      themes: ['github-dark']
+    },
+    pagination: false,
+    lastUpdated: true
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
