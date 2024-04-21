8c9990c7d065c36323ca30da465942e857ab7714
Louis Escher <louisescher@proton.me>
2024-04-21 14:29:52 +0200

    Re-add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://louisescher.github.io',
+  base: 'starlight-ion-theme',
   integrations: [starlight({
     title: 'Ion Theme',
     social: {
